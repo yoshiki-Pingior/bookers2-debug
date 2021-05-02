1292eb11dd40bbb209b4580735a0a79e5582c7e2
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-05-02 05:59:41 +0000

    [Start]Issues3

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
- 
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
