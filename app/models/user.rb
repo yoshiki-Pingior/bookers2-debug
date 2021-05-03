a2af6cbfb32cc714c76c83e7ff2d8aafa1744fe3
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-05-03 10:16:24 +0000

    [Add]MVC_follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
+  
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower_user, through: :followed, source: :follower
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50 }
