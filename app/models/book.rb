51a487fa4f8d3ba650263a9abd169ebabe2ec8f0
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-05-05 16:24:08 +0000

    [error]ajex

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
 	has_many :book_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 
-	def favorited_by?(user)
+  def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
